6677418944eede200fc90446437cd1aac08a127f
Abi-Nf <hei.abigail.3@gmail.com>
2024-12-19 09:27:33 +0300

    chore: add captcha display checker

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export function App() {
 
   const { register, handleSubmit, formState } = useForm<Form>();
   const [messages, setMessage] = useState<number[]>([]);
+  const [shouldCaptcha, setShouldRenderCaptcha] = useState(false);
   const [last_stop, setStop] = useState(0);
 
   const sendWhoami = async (i: number) => {
@@ -38,6 +39,7 @@ export function App() {
   const displayCaptcha = () => {
     renderCaptcha(ref.current!, import.meta.env.VITE_API_KEY, {
       onSuccess: async () => {
+        setShouldRenderCaptcha(false);
         await sendRequests(last_stop);
       }
     });
@@ -51,6 +53,7 @@ export function App() {
         .catch((error: AxiosError) => {
           if (error.status === 405){
             displayCaptcha();
+            setShouldRenderCaptcha(true);
             setStop(i);
             clearInterval(interval);
           }
@@ -70,20 +73,21 @@ export function App() {
   return (
     <div>
       {
-        formState.isSubmitSuccessful
-          ? messages.map(v => <div key={v}>{v}. Forbidden</div>)
-          : (
-            <form onSubmit={handleSubmit(submitCount)}>
-              <input {...register(
-                'num',
-                { required: true, valueAsNumber: true }
-                )}
-              />
-              <button type="submit">Submit</button>
-            </form>
-          )
+        !shouldCaptcha ? (
+          formState.isSubmitSuccessful
+            ? messages.map(v => <div key={v}>{v}. Forbidden</div>)
+            : (
+              <form onSubmit={handleSubmit(submitCount)}>
+                <input {...register(
+                  'num',
+                  { required: true, valueAsNumber: true }
+                  )}
+                />
+                <button type="submit">Submit</button>
+              </form>
+            )
+        ) : <div id="captcha_container" ref={ref}></div>
       }
-      <div id="captcha_container" ref={ref}></div>
     </div>
   );
 }
